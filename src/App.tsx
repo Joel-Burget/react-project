b10944fb492f8d1215aa8d105abe37ad3d874254
Joel <134548469+Joel-Burget@users.noreply.github.com>
2025-03-20 10:46:15 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+
+function App() {
+  const [alertVisible, setAlertVisible] = useState(false);
+
+  return (
+    <div>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisible(false)}>
+          You've been alerted
+        </Alert>
+      )}
+      <Button color="secondary" onClick={() => setAlertVisible(true)}>
+        My Button
+      </Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Message.tsx
@@ -0,0 +1,6 @@
+function Message() {
+  const name = "Joel";
+  return <h1>Hello {name}</h1>;
+}
+
+export default Message;
