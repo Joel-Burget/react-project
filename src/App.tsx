58ad61dc6d6d178e3ae7c00e4ec5778ff5b83449
Joel <134548469+Joel-Burget@users.noreply.github.com>
2025-04-22 11:36:59 -0600

    Added new CSS files and modules, moved ListGroup to it's own folder with an index

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import { useState } from "react";
+import ListGroup from "./components/ListGroup";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
 
 function App() {
   const [alertVisible, setAlertVisible] = useState(false);
+  const items = ["New York", "Los Angles", "San Francisco"];
 
   return (
     <div>
+      <ListGroup
+        items={items}
+        heading={""}
+        onSelectItem={function (): void {
+          throw new Error("Function not implemented.");
+        }}
+      ></ListGroup>
       {alertVisible && (
         <Alert onClose={() => setAlertVisible(false)}>
           You've been alerted
