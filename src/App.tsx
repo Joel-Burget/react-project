46a9fdfa6502501d3084522bf4757a569c26ccd3
Joel <134548469+Joel-Burget@users.noreply.github.com>
2025-05-22 12:16:02 -0600

    Added Chakra, removed unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,25 @@
-import { useState } from "react";
-import ListGroup from "./components/ListGroup";
-import Alert from "./components/Alert";
-import Button from "./components/Button";
+import { Button, ButtonGroup, Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-  const [alertVisible, setAlertVisible] = useState(false);
-  const items = ["New York", "Los Angles", "San Francisco"];
-
   return (
-    <div>
-      <ListGroup
-        items={items}
-        heading={""}
-        onSelectItem={function (): void {
-          throw new Error("Function not implemented.");
-        }}
-      ></ListGroup>
-      {alertVisible && (
-        <Alert onClose={() => setAlertVisible(false)}>
-          You've been alerted
-        </Alert>
-      )}
-      <Button color="secondary" onClick={() => setAlertVisible(true)}>
-        My Button
-      </Button>
-    </div>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
   );
 }
 
