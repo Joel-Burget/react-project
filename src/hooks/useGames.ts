afb2ca3b1d3584a3a6b1ad7171d5ed7631eceeb6
Joel <134548469+Joel-Burget@users.noreply.github.com>
2025-05-23 17:53:43 -0600

    add emojis

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -12,10 +12,10 @@ export interface Game {
   id: number;
   name: string;
   background_image: string;
-  parent_platforms: {platform: Platform}[]
-  metacritic: number
+  parent_platforms: {platform: Platform}[];
+  metacritic: number;
+  rating_top: number;
 }
-
 
 const useGames = (gameQuery: GameQuery) => 
   useData<Game>('/games', {
